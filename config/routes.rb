9cd00ff4a80c40f24cc7d476964b25e72861f298
Jim Cross <gymx@yahoo.com>
2015-12-07 14:21:24 -0700

    Add current_user helper function and changed main nav to login/out based on user session

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   resources :users
+  get   'logout'  => 'sessions#destroy'
   post  'login'   => 'sessions#create'
   get   'login'   => 'sessions#new'
   get   'signup'  => 'users#new'
