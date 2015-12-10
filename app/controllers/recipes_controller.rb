e82bbb9a61b775295401db4770089e630810d826
Jim Cross <gymx@yahoo.com>
2015-12-10 10:33:36 -0700

    Add recipes#create action, recipes_params private action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,21 +1,42 @@
 class RecipesController < ApplicationController
   def home
   end
+
   def about
   end
+
 	def index
+		@recipes = Recipe.find
 	end
+
 	def create
-		render plain: params[:recipe].inspect
+		@recipe = Recipe.new(recipe_params)
+
+		if @recipe.save
+			redirect_to @recipe
+		else
+			redirect_to new_recipe_path
+		end
 	end
+
 	def new
 	end
+
 	def edit
 	end
+
 	def show
 	end
+
 	def update
 	end
+
 	def destroy
 	end
+
+	private
+	def recipe_params
+		params.require(:recipe).permit(:title, :course, :main_ingredient, :source, :description, :ingredients, :directions, :prep_time, :cook_time, :total_time, :servings)
+	end
+
 end
