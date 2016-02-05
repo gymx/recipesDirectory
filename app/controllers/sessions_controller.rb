8d9a1be9ccf29c1f1b7703c1e6014a8b03317ffa
Jim Cross <gymx@yahoo.com>
2016-02-04 18:20:45 -0700

    Add remember me functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,10 @@ class SessionsController < ApplicationController
   def create
   	user = User.find_by(email: params[:session][:email].downcase)
   	if user && user.authenticate(params[:session][:password])
-  		log_in(user)
-  		redirect_to recipes_path
-  		#log the use in and redirect to recipe index
+  		#log the user in and redirect to recipe index
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_to recipes_path
   	else
   		# Display error message and route to 'new user page'
   		flash.now[:danger] = "Invalid email/password combination"
@@ -16,7 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_path
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160123224237) do
+ActiveRecord::Schema.define(version: 20160205003419) do
 
   create_table "recipes", force: true do |t|
     t.string   "title"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20160123224237) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
